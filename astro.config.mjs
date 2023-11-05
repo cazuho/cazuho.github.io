bcecf568170a58a8c04f1a78fc291e5a4e28dc13
ohuzak <cakumubom9210@gmail.com>
2023-11-05 14:36:59 +0900

    add new pages (about and blog)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
+// https://astro.build/config
 export default defineConfig({
     site: 'https://ohuzak.github.io',
-    base: '/blog',
-})
+    base: '/',
+});
